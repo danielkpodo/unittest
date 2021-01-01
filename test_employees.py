5e3d7c6bbb4d018028cf88754cbb63c501a1a88f
narh <kpododanielnarh@gmail.com>
2021-01-01 17:48:42 +0000

    added testcase for testing employees email and fullname

--- test_employees.py
+++ test_employees.py
@@ -3,12 +3,19 @@ from employees import Employee
 
 
 class TestEmployee(unittest.TestCase):
-    emp_1 = Employee("narh", "kpodo", 80)
-    emp_2 = Employee("nelson", "mandela", 800)
 
     def test_email(self):
-        self.assertEqual(self.emp_1.email, "narhkpodo@gmail.com")
-        self.assertEqual(self.emp_2.email, "nelsonmandela@gmail.com")
+        emp_1 = Employee("narh", "kpodo", 80)
+        emp_2 = Employee("nelson", "mandela", 800)
+        self.assertEqual(emp_1.email, "narhkpodo@gmail.com")
+        self.assertEqual(emp_2.email, "nelsonmandela@gmail.com")
+
+    def test_fullname(self):
+        emp_1 = Employee("narh", "kpodo", 320)
+        emp_2 = Employee("nelson", "mandela", 90)
+
+        self.assertEqual(emp_1.fullname, "narh kpodo")
+        self.assertEqual(emp_2.fullname, "nelson mandela")
 
 
 unittest.main()
