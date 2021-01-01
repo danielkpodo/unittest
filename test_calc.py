183303947a3bc31b047c0b9041b9794b8991bbcb
narh <kpododanielnarh@gmail.com>
2021-01-01 17:15:05 +0000

    added test options for testing substract, divide and multiply functions

--- test_calc.py
+++ test_calc.py
@@ -10,6 +10,19 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(calc.add(1, -1), 0)
         self.assertEqual(calc.add(-10, -30), -40)
 
+    def test_multiply(self):
+        self.assertEqual(calc.multiply(2, 2), 4)
+        self.assertEqual(calc.multiply(10, 0), 0)
+        self.assertEqual(calc.multiply(100, 10), 1000)
+
+    def test_substract(self):
+        self.assertEqual(calc.substract(100, 50), 50)
+        self.assertEqual(calc.substract(90, 60), 30)
+
+    def test_divide(self):
+        self.assertEqual(calc.divide(100, 2), 50)
+        self.assertEqual(calc.divide(10, 10), 1)
+
 
 # helps  us run our test_case.py file via the unittest module
 unittest.main()
