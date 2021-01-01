579efd887938875d2881d525982b331f9d311ece
narh <kpododanielnarh@gmail.com>
2021-01-01 17:03:57 +0000

    added multiple edgecases for testing the add function

--- test_calc.py
+++ test_calc.py
@@ -7,6 +7,9 @@ class TestCalc(unittest.TestCase):
     def test_add(self):
         result = calc.add(10, 90)
         self.assertEqual(result, 100)
+        self.assertEqual(calc.add(1, -1), 0)
+        self.assertEqual(calc.add(-10, -30), -40)
 
 
+# helps  us run our test_case.py file via the unittest module
 unittest.main()
