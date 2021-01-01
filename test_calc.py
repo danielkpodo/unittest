f8bfad84c681719730c46462da641b18059c876e
narh <kpododanielnarh@gmail.com>
2021-01-01 16:54:45 +0000

    added a test_case for checking an add function

--- test_calc.py
+++ test_calc.py
@@ -1,1 +1,12 @@
 import unittest
+import calc
+
+
+class TestCalc(unittest.TestCase):
+
+    def test_add(self):
+        result = calc.add(10, 90)
+        self.assertEqual(result, 100)
+
+
+unittest.main()
