448155ff9e3201e2cd7fa0ca0622e8c9035d1fb8
narh <kpododanielnarh@gmail.com>
2021-01-05 00:00:16 +0000

    added email test

---
+++ ztm/mail_checker.py
@@ -0,0 +1,2 @@
+def get_user_email(username, age):
+    return '{}.{}@gmail.com'.format(username, age)
--- ztm/test_mail.py
+++ ztm/test_mail.py
@@ -1,2 +1,12 @@
-def get_user_email(username, age):
-    return '{}.{}@gmail.com'.format(username, age)
+import unittest
+from mail_checker import get_user_email
+
+
+class TestEmail(unittest.TestCase):
+    def test_email(self):
+        username = 'narh',
+        age = 23
+        result = get_user_email(username, age)
+        self.assertEqual(result, "narh.23@gmail.com")
+
+    
